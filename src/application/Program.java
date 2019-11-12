db08dd36523330aea21afe9f036fdeef1a45ff45
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-12 10:45:09 -0300

    clear

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,15 @@
 package application;
 
-import java.math.BigDecimal;
 import java.sql.Connection;
-import java.time.LocalDate;
 
 import db.DB;
-import model.entities.Department;
-import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				
+
 		Connection connection = DB.getConnection();
 		DB.closeConnection();
 	}
-
 }
