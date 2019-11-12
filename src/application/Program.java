6e1b9f61e66850334364668a0500ed79ba1d56c7
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-12 09:09:38 -0300

    Created Department Class.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,15 @@
 package application;
 
 import java.sql.Connection;
+
 import db.DB;
-import db.DbException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Connection connection = DB.getConnection();
 		DB.closeConnection();
 	}
