dc1afc9ec2e99ee7f9824b192f93c15fdc48576f
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-12 09:44:46 -0300

    Created seller class.

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,144 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Seller implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String emailString;
+	private LocalDate birthDate;
+	private BigDecimal baseSalary;
+	
+	private Department department;
+	
+	public Seller() {
+		
+	}
+
+	public Seller(Integer id, String name, String emailString, LocalDate birthDate, BigDecimal baseSalary,
+			Department department) {
+		this.id = id;
+		this.name = name;
+		this.emailString = emailString;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailString() {
+		return emailString;
+	}
+
+	public void setEmailString(String emailString) {
+		this.emailString = emailString;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public BigDecimal getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(BigDecimal baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((baseSalary == null) ? 0 : baseSalary.hashCode());
+		result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
+		result = prime * result + ((department == null) ? 0 : department.hashCode());
+		result = prime * result + ((emailString == null) ? 0 : emailString.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		if (baseSalary == null) {
+			if (other.baseSalary != null)
+				return false;
+		} else if (!baseSalary.equals(other.baseSalary))
+			return false;
+		if (birthDate == null) {
+			if (other.birthDate != null)
+				return false;
+		} else if (!birthDate.equals(other.birthDate))
+			return false;
+		if (department == null) {
+			if (other.department != null)
+				return false;
+		} else if (!department.equals(other.department))
+			return false;
+		if (emailString == null) {
+			if (other.emailString != null)
+				return false;
+		} else if (!emailString.equals(other.emailString))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", emailString=" + emailString + ", birthDate=" + birthDate
+				+ ", baseSalary=" + baseSalary + ", department=" + department + "]";
+	}
+		
+}
