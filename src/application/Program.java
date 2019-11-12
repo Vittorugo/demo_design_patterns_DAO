5d69cd729993b97f5bbdc7ab0ff6de798b5ca67b
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-12 13:33:30 -0300

    Test find by id

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,24 @@
 package application;
 
-import java.sql.Connection;
+import java.util.Locale;
 
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Connection connection = DB.getConnection();
+		
+		Locale.setDefault(Locale.US);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDaO();
+		
+		 Seller seller = sellerDao.findById(10);
+		
+		System.out.println(seller);
+		
 		DB.closeConnection();
 	}
 }
