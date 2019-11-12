a92c9be9b0dd394519b37d1d5b409da99641c2c9
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-12 10:38:12 -0300

    Dao Factory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDaO() {
+		return new SellerDaoJDBC();
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC();
+	}
+}
