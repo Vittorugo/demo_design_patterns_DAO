1b618330f1c8e1ce358f16a580e53469c5725c2f
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-12 13:39:32 -0300

    find by id method implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,26 @@
 package model.dao.impl;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
-
+	
+	private Connection connection;
+	
+	public SellerDaoJDBC(Connection connection) {
+		this.connection = connection;
+	}
+	
 	@Override
 	public void insert(Seller seller) {
 
@@ -24,7 +38,40 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		return null;
+		
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		try {
+			preparedStatement = connection.prepareStatement("SELECT seller.*, department.Name AS DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id WHERE seller.Id = ?");
+			
+			preparedStatement.setInt(1, id);
+			
+			resultSet = preparedStatement.executeQuery(); // return search result
+			
+			if(resultSet.next()) { // true if result > 0
+				Department department = new Department();
+				department.setId(resultSet.getInt("DepartmentId")); // table return for department id instance
+				department.setName(resultSet.getString("DepName")); // table return for department name instance
+				
+				Seller seller = new Seller();
+				seller.setId(resultSet.getInt("Id"));
+				seller.setName(resultSet.getString("Name"));
+				seller.setEmailString(resultSet.getString("Email"));
+				seller.setBirthDate(new java.sql.Date((resultSet.getDate("BirthDate").getTime())).toLocalDate()); // convert sql date in localdate
+				seller.setBaseSalary(BigDecimal.valueOf(resultSet.getDouble("BaseSalary"))); // convert sql double in BigDecimal
+				seller.setDepartment(department); // obj department
+				
+				return seller;
+			}
+			
+			return null;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(preparedStatement);
+			DB.closeResultSet(resultSet);
+		}	
 	}
 
 	@Override
