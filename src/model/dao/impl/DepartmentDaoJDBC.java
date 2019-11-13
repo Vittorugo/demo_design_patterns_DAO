f20e81f82ddd0490c6d8b09f762e5eb58577056a
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-13 20:46:33 -0300

    insert method implementation

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,15 +1,44 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
-
+	
+	private Connection connection = null;
+	
+	public DepartmentDaoJDBC(Connection connection){
+		this.connection = connection;
+	}
+	
 	@Override
 	public void insert(Department department) {
-
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = connection.prepareStatement("INSERT INTO department (Id, Name) values (?,?)");
+			
+			preparedStatement.setInt(1, department.getId());
+			preparedStatement.setString(2, department.getName());
+			
+			int rowsAffected = preparedStatement.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				System.out.println("Department inserted!");
+			} else {
+				throw new DbException("Error! Unable insert department!");
+			}
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(preparedStatement);
+		}
 	}
 
 	@Override
